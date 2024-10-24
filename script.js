00b4c36bf8a9ce98ced39f442fe421952d2dbf1e
libresoul <contact@damindu.me>
2024-10-24 16:18:32 +0530

    display errors in spans

--- script.js
+++ script.js
@@ -23,28 +23,35 @@ function FormSubmit(event) {
 
     if (name === "") {
         errorMessages.push("Name cannot be empty");
+        document.getElementById("nameError").textContent = "Name cannot be empty";
     }
     else if (!nameRegex.test(name)) {
         errorMessages.push("Invalid Name: Name should only contain letters and spaces");
+        document.getElementById("nameError").textContent = "Invalid Name: Name should only contain letters and spaces";
     }
 
     if (address === "") {
         errorMessages.push("Address cannot be empty");
+        document.getElementById("addrError").textContent = "Address cannot be empty";
     }
 
     if (dob === "") {
         errorMessages.push("Date of birth cannot be empty");
+        document.getElementById("dobError").textContent = "Date of birth cannot be empty";
     }
 
     if (age === "") {
         errorMessages.push("Age must be a number");
+        document.getElementById("ageError").textContent = "Age must be a number";
     }
     else if (!ageRegex.test(age)) {
         errorMessages.push("Invalid age")
+        document.getElementById("ageError").textContent = "Invalid age";
     }
 
     if (!genderFemale && !genderMale) {
         errorMessages.push("Select at least one gender");
+        document.getElementById("genderError").textContent = "Select at least one gender";
     }
     else if (genderMale) {
         gender = "Male";
@@ -53,22 +60,27 @@ function FormSubmit(event) {
         gender = "Female";
     }
 
-    if (disease === "") {
+    if (disease === "0") {
         errorMessages.push("Select a disease");
+        document.getElementById("diseaseError").textContent = "Select a disease";
     }
 
     if (email === "") {
         errorMessages.push("Email cannot be empty");
+        document.getElementById("emailError").textContent = "Email cannot be empty";
     }
     else if (!emailRegex.test(email)) {
         errorMessages.push("Invalid email");
+        document.getElementById("emailError").textContent = "Invalid email";
     }
 
     if (pin.length !== 6) {
         errorMessages.push("Pin should contain 6 digits");
+        document.getElementById("pinError").textContent = "Pin should contain 6 digits";
     }
     else if (!pinRegex.test(pin)) {
         errorMessages.push("Pin should only contain digits");
+        document.getElementById("pinError").textContent = "Pin should only contain digits";
     }
 
     if (errorMessages.length > 0) {
